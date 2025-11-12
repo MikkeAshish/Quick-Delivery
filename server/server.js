47a4c6072f3e8411aa1a8b8c1ba256629b721ec4
MikkeAshish <kumawatmikke2003@gmail.com>
2025-11-12 10:34:05 +0530

    third commit

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ await connectDB()
 await connectCloudinary()
 
 // Allow multiple origins
-const allowedOrigins = ['http://localhost:5174', '']
+const allowedOrigins = ['http://localhost:5174', 'https://quick-delivery-z6ju.vercel.app']
 
 
 // Middleware configuration
